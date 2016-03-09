57b56887664fb3959f83a797d8892b98d66cd95e
Andreas Åkre Solberg <andreas.solberg@uninett.no>
2016-03-09 12:00:04 +0100

    Push script instead of build script

--- bin/push.sh
+++ bin/push.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
-
-
-PACKAGE=gcr.io/solberg-cluster/smedstua
-
-echo "Building image ${PACKAGE}:${TRAVIS_COMMIT}"
-
-docker build -t ${PACKAGE}:${TRAVIS_COMMIT}
-
+#
 echo "Working on branch ${TRAVIS_BRANCH}"
 
 if [ "$TRAVIS_BRANCH" = "master" ]; then
@@ -18,8 +11,3 @@ if [ "$TRAVIS_BRANCH" = "stable" ]; then
     echo "Tagging MASTER ${PACKAGE}:${TRAVIS_COMMIT} as ${PACKAGE}:stable"
     docker tag ${PACKAGE}:${BUILD_TAG} ${PACKAGE}:stable
 fi
-
-
-# docker tag ${PACKAGE}:${BUILD_TAG} ${PACKAGE}:unstable
-# echo $TRAVIS_BRANCH
-# echo ${TRAVIS_COMMIT}
