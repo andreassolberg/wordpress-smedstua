6e12f3236ea3bf31fdf400f396f666e47b31353e
Andreas Åkre Solberg <andreas.solberg@uninett.no>
2016-03-09 11:45:29 +0100

    Separating out build script into separate file

---
+++ bin/build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+
+PACKAGE=gcr.io/solberg-cluster/smedstua
+
+echo "Building image ${PACKAGE}:${TRAVIS_COMMIT}"
+
+docker build -t ${PACKAGE}:${TRAVIS_COMMIT}
+
+
+if [ "$TRAVIS_BRANCH" = "master" ]
+    echo "Tagging MASTER ${PACKAGE}:${TRAVIS_COMMIT} as ${PACKAGE}:unstable"
+    docker tag ${PACKAGE}:${BUILD_TAG} ${PACKAGE}:unstable
+fi
+
+if [ "$TRAVIS_BRANCH" = "stable" ]
+    echo "Tagging MASTER ${PACKAGE}:${TRAVIS_COMMIT} as ${PACKAGE}:stable"
+    docker tag ${PACKAGE}:${BUILD_TAG} ${PACKAGE}:stable
+fi
+
+
+# docker tag ${PACKAGE}:${BUILD_TAG} ${PACKAGE}:unstable
+# echo $TRAVIS_BRANCH
+# echo ${TRAVIS_COMMIT}
