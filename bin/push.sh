ae8d1e981fae05c26acd017a9dbdb8cb498c4acb
Andreas Åkre Solberg <andreas.solberg@uninett.no>
2016-03-09 12:08:01 +0100

    Fix use of env

--- bin/push.sh
+++ bin/push.sh
@@ -4,10 +4,10 @@ echo "Working on branch ${TRAVIS_BRANCH}"
 
 if [ "$TRAVIS_BRANCH" = "master" ]; then
     echo "Tagging MASTER ${PACKAGE}:${TRAVIS_COMMIT} as ${PACKAGE}:unstable"
-    docker tag ${PACKAGE}:${BUILD_TAG} ${PACKAGE}:unstable
+    docker tag ${PACKAGE}:${TRAVIS_COMMIT} ${PACKAGE}:unstable
 fi
 
 if [ "$TRAVIS_BRANCH" = "stable" ]; then
     echo "Tagging MASTER ${PACKAGE}:${TRAVIS_COMMIT} as ${PACKAGE}:stable"
-    docker tag ${PACKAGE}:${BUILD_TAG} ${PACKAGE}:stable
+    docker tag ${PACKAGE}:${TRAVIS_COMMIT} ${PACKAGE}:stable
 fi
